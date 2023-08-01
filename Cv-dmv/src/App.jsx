dff72d8216d7163808a72037428015066b1d8678
JUANKAMILO2005 <ojedavalenciajuancamilo@gmail.com>
2023-07-31 19:42:11 -0500

    Funciona y la rutas tambien solo falta poner lo bonito.DMV

--- Cv-dmv/src/App.jsx
+++ Cv-dmv/src/App.jsx
@@ -1,30 +1,15 @@
-import '../src/App.css'
-import { Header } from './components/Header/Header';
-import { Profile } from './components/Profile/Profile';
-import { Academic } from './components/Academic/Academic';
-import { Experience } from './components/Experience/Experience';
-import { Skills } from './components/Skills/Skills';
-import { Interest } from './components/Interest/Interest';
-import { Languages } from './components/Languages/Languages';
-
+import "../src/App.css";
+import  Portafolio  from "../src/Pages/Portafolio";
+import { Routes, Route } from "react-router-dom";
+import  Home  from "../src/Pages/Home";
 
 function App() {
-
   return (
-    <div className='App'>
-      <Header />
-      <Profile />
-      <Experience />
-      <div className="container-Academic-Skills">
-        <Academic />
-        <Skills />
-      </div>
-      <div className="container-Interest-languages">
-        <Interest />
-        <Languages />
-      </div>
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/portafolio" element={<Portafolio />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- Cv-dmv/src/Pages/Portafolio.jsx
+++ Cv-dmv/src/Pages/Portafolio.jsx
@@ -1,8 +1,28 @@
-import { Router,Route } from "react-router-dom";
+import { Header } from '../components/Header/Header';
+import { Profile } from '../components/Profile/Profile';
+import { Academic } from '../components/Academic/Academic';
+import { Experience } from '../components/Experience/Experience';
+import { Skills } from '../components/Skills/Skills';
+import { Interest } from '../components/Interest/Interest';
+import { Languages } from '../components/Languages/Languages';
+
 
 function Portafolio() {
+
   return (
-    
+    <div className='App'>
+      <Header />
+      <Profile />
+      <Experience />
+      <div className="container-Academic-Skills">
+        <Academic />
+        <Skills />
+      </div>
+      <div className="container-Interest-languages">
+        <Interest />
+        <Languages />
+      </div>
+    </div>
   )
 }
 
